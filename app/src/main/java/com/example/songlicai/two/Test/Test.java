58747337d9e7c71e51e1265952e892b057812ee1
lnslc <lnslc@126.com>
2016-11-21 17:56:24 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/example/songlicai/two/Test/Test.java
@@ -0,0 +1,14 @@
+package com.example.songlicai.two.Test;
+
+/**
+ * Created by songlicai on 2016/11/21.
+ */
+
+public class Test
+{
+    public String getValue()
+    {
+        return "xyz";
+    }
+
+}
